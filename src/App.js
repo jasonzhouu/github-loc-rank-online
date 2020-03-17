904a2b9e5da7f3195ec55125ce708763dd87ef08
Yusheng Zhou 周宇盛 <jasonzhouu@gmail.com>
2020-03-17 10:36:38 +0800

    select

--- src/App.js
+++ src/App.js
@@ -1,39 +1,55 @@
-import React, {Component} from 'react';
-import SortButton from './components/SortButton'
-import './App.css';
+import React, { Component } from "react";
+import SortButton from "./components/SortButton";
+import FilterSelect from "./components/FilterSelect";
+import "./App.css";
 
 class App extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
+      filter: "",
       sort: {
-        type: '',
-        direction: ''
-      }
-    }
-    this.sort = this.sort.bind(this)
+        type: "",
+        direction: ""
+      },
+      languageList: [
+        { name: "javascript", count: 1 },
+        { name: "java", count: 2 }
+      ]
+    };
+    this.sort = this.sort.bind(this);
+    this.filter = this.filter.bind(this);
   }
   sort(type) {
     let direction;
-    if(this.state.sort.type === type) {
-      if(this.state.sort.direction === 'ascending') {
-        direction = 'descending'
-      } else if(this.state.sort.direction === 'descending') {
-        direction = 'ascending'
+    if (this.state.sort.type === type) {
+      if (this.state.sort.direction === "ascending") {
+        direction = "descending";
+      } else if (this.state.sort.direction === "descending") {
+        direction = "ascending";
       }
     } else if (this.state.sort.type !== type) {
-      direction = 'ascending'
+      direction = "ascending";
     }
     this.setState({
       sort: {
         type,
         direction
       }
-    })
+    });
+  }
+  filter(event) {
+    this.setState({
+      filter: event.target.value
+    });
   }
   render() {
     return (
       <div>
+        <FilterSelect
+          languageList={this.state.languageList}
+          filter={this.filter}
+        />
         <SortButton sort={this.sort} sortState={this.state.sort}>
           LOC
         </SortButton>
---
+++ src/components/FilterSelect.js
@@ -0,0 +1,28 @@
+import React, { PureComponent } from "react";
+
+class FilterOption extends PureComponent {
+  render() {
+    const { option } = this.props;
+    return (
+      <option value={option.name}>
+        {option.name} -- {option.count}
+      </option>
+    );
+  }
+}
+
+class FilterSelect extends PureComponent {
+  render() {
+    const { languageList, filter } = this.props;
+    return (
+      <select onChange={filter}>
+        <option value="-">--language--</option>
+        {languageList.map(option => (
+          <FilterOption option={option} key={option.name} />
+        ))}
+      </select>
+    );
+  }
+}
+
+export default FilterSelect;
